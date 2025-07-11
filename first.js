7e4c7926e40961d777d1874bcaec3007e4ebe807
Rejaul Islam <islamrejaul130@gmail.com>
2025-07-11 23:38:45 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,18 @@
+const button = "Added a Button";
+console.log(button);
+
+const login = "Login page added";
+console.log(login);
+
+const footer = "Footer added in our website";
+console.log(footer);
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
+
+const upi = "Integrated the UPI";
+console.log(upi);
+
+console.log("Latest Update");
+
+// I am fixing some Bug
+console.log("Bug Fixed");
